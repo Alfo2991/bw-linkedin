e5b2d04d38369c9fe04ae87118ac8ee6493201c8
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-04-17 17:24:00 +0200

    d1

--- src/components/myNav.jsx
+++ src/components/myNav.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import * as Icon from "react-bootstrap-icons";
-import {Row,Col,Form,NavDropdown} from "react-bootstrap";
+import {Form} from "react-bootstrap";
 
 
 function MyNav() {
@@ -82,19 +82,15 @@ function MyNav() {
             </Nav.Link>
             </div>
 
-            {/* Per le aziende */}
-            <div className='icons'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-grid-3x3-gap grid3" viewBox="0 0 16 16">
-            <path d="M4 2v2H2V2h2zm1 12v-2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm5 10v-2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zM9 2v2H7V2h2zm5 0v2h-2V2h2zM4 7v2H2V7h2zm5 0v2H7V7h2zm5 0h-2v2h2V7zM4 12v2H2v-2h2zm5 0v2H7v-2h2zm5 0v2h-2v-2h2zM12 1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zm1 4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-2z"/>
-            </svg>
-            <Nav.Link href="#features" className='text5'>Per le aziende</Nav.Link>
-            </div>
-
-        
-
         </Nav>
 
-      
+        <div>
+
+            {/* Partizione */}
+            <div className='Partizione'>
+            </div>
+
+        </div>
         </Container>
         
       </Navbar>
