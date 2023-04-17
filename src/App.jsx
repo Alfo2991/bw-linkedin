54c17a3b879e4581988344b82efe9a487efa5f43
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-04-17 15:50:15 +0200

    Linkedin BW

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import MyNav from './components/myNav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <MyNav/>
+    </div>
+  );
+}
+
+export default App;
