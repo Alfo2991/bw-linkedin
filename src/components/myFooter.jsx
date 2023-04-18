6633584e3c3b3cba54c6aa2493921ababc3bc1a4
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-04-18 15:38:59 +0200

    Insights

--- src/components/myFooter.jsx
+++ src/components/myFooter.jsx
@@ -78,12 +78,11 @@ const MyFooter = () => {
               >
                 <path d="M12 2a10 10 0 1010 10A10 10 0 0012 2zm0 16.25A1.25 1.25 0 1113.25 17 1.25 1.25 0 0112 18.25zm1.41-5.46L13 13v1h-2v-2.21l1.49-.79C13.82 10.34 14 9.77 14 9.3c0-.78-.92-1.3-2.3-1.3A7.12 7.12 0 008 9.24V7a8 8 0 013.7-1c3 0 4.3 1.55 4.3 3.3a3.91 3.91 0 01-2.59 3.49z"></path>
               </svg>
-              </div>
-              <div className="d-flex flex-column">
-                <span>Domande?</span>
-                <span>Visita il nostro Centro assistenza.</span>
-              </div>
-            
+            </div>
+            <div className="d-flex flex-column">
+              <span>Domande?</span>
+              <span>Visita il nostro Centro assistenza.</span>
+            </div>
           </div>
           <div className="d-flex ">
             <div className="d-flex align-items-center">
@@ -99,12 +98,11 @@ const MyFooter = () => {
               >
                 <path d="M9.18 2l-4.3 2.52L6.22 8l-.52.91-3.7.55v5l3.64.54.54.93-1.34 3.53L9.14 22l2.29-2.9h1.09l2.3 2.9 4.32-2.52L17.79 16l.53-.93 3.68-.53v-5L18.32 9l-.51-.9 1.36-3.51L14.86 2l-2.33 3h-1zM12 9a3 3 0 11-3 3 3 3 0 013-3z"></path>
               </svg>
-              </div>
-              <div className="d-flex flex-column">
-                <span>Gestisci il tuo account e la tua privacy</span>
-                <span>Vai alle impostazioni</span>
-              </div>
-           
+            </div>
+            <div className="d-flex flex-column">
+              <span>Gestisci il tuo account e la tua privacy</span>
+              <span>Vai alle impostazioni</span>
+            </div>
           </div>
           <div className="d-flex t">
             <div className="d-flex align-items-center">
@@ -120,12 +118,11 @@ const MyFooter = () => {
               >
                 <path d="M12 4.11V19.6l-3-2a9 9 0 01-4-7.49V6.44l7-2.33M12 2L3 5v5.11a11 11 0 004.9 9.16L12 22l4.1-2.73a11 11 0 004.9-9.16V5z"></path>
               </svg>
-              </div>
-              <div className="d-flex flex-column">
-                <span>Trasparenza sui contenuti consigliati</span>
-                <span>Scopri di più sui contenuti consigliati.</span>
-              </div>
-            
+            </div>
+            <div className="d-flex flex-column">
+              <span>Trasparenza sui contenuti consigliati</span>
+              <span>Scopri di più sui contenuti consigliati.</span>
+            </div>
           </div>
         </Col>
         <Col lg={3}>
@@ -149,4 +146,3 @@ const MyFooter = () => {
 };
 
 export default MyFooter;
-
