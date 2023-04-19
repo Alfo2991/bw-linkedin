29d2af71c07708ad21996644235e1b939bc5503b
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-04-19 13:31:00 +0200

    d3

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TopProfile from "./components/TopProfile";
 
 import { Container, Row, Col, Form, Dropdown } from "react-bootstrap";
-//import TopProfile from "./components/TopProfile";
-import Profile from "./components/ProfilePage/Profile";
 
 function App() {
   return (
