8c9740336337462ef35a2830e906119b8ec6d072
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-04-19 16:03:50 +0200

    d3

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -49,38 +49,18 @@ function Resources() {
           <li className="pvs-list__item--three-column ">
             <div className="pvs-entity pvs-entity--padded pvs-list__item--no-padding-in-columns">
               <div className="mot">
-                <a
-                  data-field="insights_wvmp"
-                  className="optional-action-target-wrapper display-flex"
-                  target="_self"
-                  tabIndex="-1"
-                  aria-hidden="true"
-                  href="https://www.linkedin.com/me/profile-views"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="currentColor"
+                  class="mercado-match"
+                  width="24"
+                  height="24"
+                  focusable="false"
                 >
-                  <div className="ivm-image-view-model pvs-entity__image ">
-                    <div className="ivm-view-attr__img-wrapper ivm-view-attr__img-wrapper--use-img-tag display-flex">
-                      <li-icon
-                        aria-hidden="true"
-                        type="people"
-                        className="ivm-view-attr__icon--icon  "
-                        size="large"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          data-supported-dps="24x24"
-                          fill="currentColor"
-                          className="mercado-match"
-                          width="24"
-                          height="24"
-                          focusable="false"
-                        >
-                          <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
-                        </svg>
-                      </li-icon>
-                    </div>
-                  </div>
-                </a>
+                  <path d="M21 12h-1a9 9 0 00-9-9V2a10 10 0 0110 10zM11 5v1a6 6 0 016 6h1a7 7 0 00-7-7zm3 7h1a4 4 0 00-4-4v1a3 3 0 013 3zm-2 0a1 1 0 00-1.82-.54L5.32 6.6a8 8 0 00-.24 8.4 2.33 2.33 0 014.16.68l.88 3.08A8.57 8.57 0 0012 19a8 8 0 004.4-1.32l-4.86-4.86A1 1 0 0012 12zm-5 3a1.32 1.32 0 00-1.27 1L4 22h6l-1.73-6A1.32 1.32 0 007 15z"></path>
+                </svg>
               </div>
               <div className="display-flex align-items-center full-width">
                 <div className="display-flex flex-row justify-content-center">
@@ -105,6 +85,51 @@ function Resources() {
                         <span className="visually-hidden text-nowrap">
                           Get discovered, showcase content on your profile, and
                           get access to creator tools
+                        </span>
+                      </span>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="pvs-entity pvs-entity--padded pvs-list__item--no-padding-in-columns">
+              <div className="mot">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="currentColor"
+                  class="mercado-match"
+                  width="24"
+                  height="24"
+                  focusable="false"
+                >
+                  <path d="M12 16v6H3v-6a3 3 0 013-3h3a3 3 0 013 3zm5.5-3A3.5 3.5 0 1014 9.5a3.5 3.5 0 003.5 3.5zm1 2h-2a2.5 2.5 0 00-2.5 2.5V22h7v-4.5a2.5 2.5 0 00-2.5-2.5zM7.5 2A4.5 4.5 0 1012 6.5 4.49 4.49 0 007.5 2z"></path>
+                </svg>
+              </div>
+              <div className="display-flex align-items-center full-width">
+                <div className="display-flex flex-row justify-content-center">
+                  <a
+                    data-field="insights_wvmp"
+                    className="optional-action-target-wrapper display-flex align-items-center"
+                    target="_self"
+                    href="https://www.linkedin.com/me/profile-views"
+                  >
+                    <div className="discoverWho pvs-entity__image">
+                      <span className="mr-2 hoverable-link-text mb-1">
+                        <span aria-hidden="true" className="text-black views">
+                          My Network
+                        </span>
+                        <span className="visually-hidden">My Network</span>
+                      </span>
+                      <span className=" t-14 t-normal mb-1 text-nowrap">
+                        <span aria-hidden="true">
+                          My network See and manage your connections and
+                          interests.
+                        </span>
+                        <span className="visually-hidden text-nowrap">
+                          My network See and manage your connections and
+                          interests.
                         </span>
                       </span>
                     </div>
