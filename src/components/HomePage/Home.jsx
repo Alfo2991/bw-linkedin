5bd00059da6f056fad130eb0a43cf70d00e69cee
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-04-21 11:08:49 +0200

    now

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,12 +1,11 @@
-import "../FeaturedPosts/FeaturedPosts";
-import Profile from "../ProfilePage/Profile";
+import Profile from "../ProfilePage/Profile"
+
 
 const Home = ({ userProfile, setUserProfile }) => {
   return (
     <>
       <Profile userProfile={userProfile} setUserProfile={setUserProfile} />
       <div id="sidebar" className="pl-5 ml-5 mt-4">
-        <Sidebar />
       </div>
     </>
   );
---
+++ src/components/HomePage/NewsFeed.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import "../FeaturedPosts/FeaturedPosts";
+import SingleFeed from "./SingleFeed";
+import "../Sidebar/Sidebar.css";
+import "./Home.css";
+import SinglePost from "./SinglePost";
+import { Col, Container, Row } from "react-bootstrap";
+import RightSidebar from "./HomeRightSidebar/RightSidebar";
+
+const NewsFeed = ({ userProfile, setUserProfile }) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/posts/",
+        {
+          headers: {
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNmOWYxZTA4ZjNkNzAwMTRjM2U5ODciLCJpYXQiOjE2ODE4OTExMDMsImV4cCI6MTY4MzEwMDcwM30.T_dtXS4sdwETVn1QxaN0Er8czTLxIHXKZ40FnaiXnEI`,
+          },
+        }
+      );
+      if (response.ok) {
+        let responseJson = await response.json();
+        console.log("POSTS", responseJson);
+        setPosts(responseJson);
+        return posts;
+      } else {
+        console.log("Errore durante il recupero dei dati");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div id="feed" className="d-flex align-items-center flex-column">
+      {posts
+        .filter((post) => post.user)
+        .slice(0, 6)
+        .map((post) => (
+          <SingleFeed
+            image={post.user.image}
+            name={post.user.name}
+            surname={post.user.surname}
+            title={post.user.title}
+            text={post.text}
+            id={post._id}
+            key={post._id}
+          />
+        ))
+        .reverse()}
+    </div>
+  );
+};
+export default NewsFeed;
